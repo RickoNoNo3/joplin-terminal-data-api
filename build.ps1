d626fea3ef6fc696992efaf066084724c7def699
rickonono3 <rickonono3@qq.com>
2025-05-15 18:16:56 +0800

    prune

--- build.ps1
+++
@@ -1,2 +0,0 @@
-docker builder prune --all
-docker build -t rickonono3/joplin-terminal-data-api .
--- build.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-docker builder prune --all
-docker build -t rickonono3/joplin-terminal-data-api .
